fee29ae91baae9bb7d2dc552e67fa36266258121
Robert Karapetyan <ROBERT.KARAPETYAN@AMERIHOME.COM>
2020-02-20 10:02:58 -0800

    phase 9

---
+++ CSharp.PrimitveRefAndValTypes/DynamicPrimitveTypeTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.PrimitveRefAndValTypes
+{
+    [TestClass]
+    public class DynamicPrimitveTypeTests
+    {
+        [TestMethod]
+        public void ShouldAddDynamicTypes()
+        {
+            dynamic value;
+
+            value = 5;
+            M(value + value);
+
+            value = "Albert";
+            M(value + " Cervantes");
+        }
+
+        private static void M(int n)
+        {
+            Assert.AreEqual(typeof(int), n.GetType());
+        }
+
+        private static void M(string s)
+        {
+            Assert.AreEqual(typeof(string), s.GetType());
+        }
+    }
+}
