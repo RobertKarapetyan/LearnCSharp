ec9ea057e6bd34c6909bc09b042692747d93e949
Robert Karapetyan <ROBERT.KARAPETYAN@AMERIHOME.COM>
2020-02-20 09:35:41 -0800

    phase 8

---
+++ CSharp.PrimitveRefAndValTypes/HashCodesTests.cs
@@ -0,0 +1,27 @@
+﻿using CSharp.PrimitveRefAndValTypes.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.PrimitveRefAndValTypes
+{
+    [TestClass]
+    public class HashCodesTests
+    {
+        [TestMethod]
+        public void ShouldHaveTheSameHashCodes()
+        {
+            var ref1 = new SomeRef();
+            var ref2 = ref1;
+            
+            Assert.AreEqual(ref1.GetHashCode(), ref2.GetHashCode());
+        }
+
+        [TestMethod]
+        public void ShouldNotHaveTheSameHashCodes()
+        {
+            var ref1 = new SomeRef();
+            var ref2 = new SomeRef();
+            
+            Assert.AreNotEqual(ref1.GetHashCode(), ref2.GetHashCode());
+        }
+    }
+}
