124616f418cee49ac6e05b6f3c069c1d7cbef057
robertkarapetyan <robkarapet@gmail.com>
2020-03-10 21:32:05 -0700

    phase 22

---
+++ CSharp.Events/ExternalDelegateTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.Events
+{
+    internal delegate string MyFunction<in Tin>(Tin input);
+    
+    internal static class MyClass
+    {
+        public static readonly MyFunction<int> myFunction;
+
+        static MyClass()
+        {
+            myFunction = input => input.ToString();         
+        }
+    }
+    
+    [TestClass]
+    public class MyClassTests
+    {
+        [TestMethod]
+        public void ShouldCallDelegate()
+        {
+            Assert.AreEqual("151", MyClass.myFunction(151));
+        }
+    }
+}
---
+++ CSharp.Interfaces/VarianceTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.Interfaces
+{
+    [TestClass]
+    public class VarianceTests
+    {
+        [TestMethod]
+        public void ShouldConvertTopToBottom()
+        {
+            IMessenger<Message> messenger; 
+            
+            messenger = new Messenger<EmailMessage>();
+            var defaultMessage = messenger.Message();
+            Assert.AreEqual("CSharp.Interfaces.EmailMessage",  
+                defaultMessage.GetType().ToString());
+            Assert.AreEqual("abcd@mail.com", 
+                (defaultMessage as EmailMessage)?.EmailAddress);
+
+            messenger = new Messenger<TextMessage>();;
+            var textMessage = messenger.Message();
+            Assert.AreEqual("CSharp.Interfaces.TextMessage", 
+                textMessage.GetType().ToString());
+            Assert.AreEqual("123 456 7891", 
+                (textMessage as TextMessage)?.PhoneNumber);
+        }
+    }
+
+    internal interface IMessenger<out T> where T : IMessage
+    {
+        T Message();
+    }
+
+    internal class Messenger<T> : IMessenger<T> where T : IMessage, new()
+    {    
+        public T Message()
+        {
+            var result = new T();
+            return result;
+        }
+    }
+
+    internal interface IMessage
+    {
+        string MessageBody();
+    }
+
+    internal class Message : IMessage
+    {
+        private readonly string _messageBody;
+        
+        public Message(string messageBody)
+        {
+            _messageBody = messageBody;
+        }
+
+        protected Message()
+        {
+           
+        }
+
+        public string MessageBody()
+        {
+            var result = _messageBody;
+            return result;
+        }
+    }
+    
+    internal class EmailMessage : Message
+    {
+        public string EmailAddress { get; set; } = "abcd@mail.com";
+
+        public EmailMessage()
+        {
+        }
+    }
+
+    internal class TextMessage : Message
+    {
+        public string PhoneNumber { get; set; } = "123 456 7891";
+        public TextMessage() { }
+    }
+}
