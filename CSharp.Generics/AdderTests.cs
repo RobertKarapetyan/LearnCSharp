18d8fc94385e9b455f956c57a45cad0475ce1f63
Robert Karapetyan <ROBERT.KARAPETYAN@AMERIHOME.COM>
2020-03-11 09:55:06 -0700

    phase 25

--- CSharp.Generics/AdderTests.cs
+++ CSharp.Generics/AdderTests.cs
@@ -10,7 +10,10 @@ namespace CSharp.Generics
         public void ShouldAddInts()
         {
             var adder = new Adder<int>();
+            
             Assert.AreEqual(2, adder.Add(1, 1));
+            AdderParent<int> adderParent = adder;
+            Assert.AreEqual(2, adderParent.Add(1, 1));
         }
 
         [TestMethod]
@@ -31,32 +34,35 @@ namespace CSharp.Generics
         }
     }
 
-    internal class Adder<T> where T : IComparable
+    internal class Adder<T> : AdderParent<T> where T : IComparable 
+     {
+         public T Max(T n1, T n2)
+         {
+             T result;
+             var comparision = n1.CompareTo(n2);
+ 
+             if (comparision < 0)
+             {
+                 result = n2;
+             }
+             else if (comparision > 0)
+             {
+                 result = n1;
+             }
+             else
+             {
+                 result = default(T);
+             }
+ 
+             return result;
+         }
+     }
+
+    internal class AdderParent<T>
     {
         public T Add(dynamic n1, dynamic n2)
         {
             var result = (T) (n1 + n2);
-            return result;
-        }
-
-        public T Max(T n1, T n2)
-        {
-            T result;
-            var comparision = n1.CompareTo(n2);
-
-            if (comparision < 0)
-            {
-                result = n2;
-            }
-            else if (comparision > 0)
-            {
-                result = n1;
-            }
-            else
-            {
-                result = default(T);
-            }
-
             return result;
         }
     }
