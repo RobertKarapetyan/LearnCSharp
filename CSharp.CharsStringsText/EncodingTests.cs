f4fb2efc85b53ee70e30b1a63a8a29590169abff
Robert Karapetyan <ROBERT.KARAPETYAN@AMERIHOME.COM>
2020-03-11 18:12:47 -0700

    phase 29

---
+++ CSharp.CharsStringsText/EncodingTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.CharsStringsText
+{
+    [TestClass]
+    public class EncodingTests
+    {
+        [TestMethod]
+        public void ShouldEncode()
+        {
+            const string s = "Albert is there.";
+           
+            var encodedBytes = Encoding.UTF8.GetBytes(s);
+            Assert.AreEqual(65, encodedBytes[0]);
+            
+            var decodedString = Encoding.UTF8.GetString(encodedBytes);
+            Assert.AreEqual(s, decodedString);
+        }
+    }
+}
